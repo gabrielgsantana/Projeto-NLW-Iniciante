418c76343c4ee2e0bcb75c9d17bf884944990b03
Gabriel Santana <122163924+gabrielgsantana@users.noreply.github.com>
2024-09-13 11:38:02 -0300

    Initial commit

---
+++ estudos.js
@@ -0,0 +1,30 @@
+// arrays, objetos
+let meta = {
+   value: "ler um livro por mês",
+//   anddress:2,
+  checked: true,
+//   log: (info) => (
+//      console.log(info)
+// )
+}
+
+
+let metas = [
+    meta,
+    {
+       value: "Caminhar 20 minutos por dia.",
+       checked: false,
+    }
+]
+
+console.log(metas[0].value)
+
+
+//meta.value = "não é mais ler um livro"
+//meta.log(meta.value)
+
+// function     // arrow function
+
+//const criarMeta = () => {}
+
+//function  criarMeta() {}
